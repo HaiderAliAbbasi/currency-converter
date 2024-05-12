d12a37cc24bdb1b18c8ae57bab9d127649ae67ec
HaiderAliAbbasi <haideraliabbasi.399@gmail.com>
2024-05-12 14:03:10 +0500

    work done

---
+++ index.ts
@@ -0,0 +1,39 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+const currency: any = {
+  USD: 1, // base currency
+  EUR: 0.93,
+  GBP: 0.80,
+  INR: 83.55,
+  PKR: 278.04,
+};
+
+let user_answer = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Enter from currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "to",
+    message: "Enter to currency",
+    type: "list",
+    choices: ["USD", "EUR", "GBP", "INR", "PKR"],
+  },
+  {
+    name: "amount",
+    message: "Enter Amount",
+    type: "number",
+  },
+]);
+
+let fromAmount = currency [user_answer.from]
+let toAmount = currency [user_answer.to]
+let amount = user_answer.amount;
+let baseAmount = amount / fromAmount; // USD base currency
+let convertedAmount = baseAmount * toAmount;
+
+console.log(convertedAmount);
